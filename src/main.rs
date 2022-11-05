a14ed82b0b55f8f0221c90428d2532ec2f2d1a8e
Aurora <auroraesmerelda@gmail.com>
2022-11-05 18:00:52 +1100

    Actually ping servers and structs for data

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use std::{io::Result, fs::File};
-use serde::Deserialize;
+use std::{io::{Error, Result, ErrorKind}, fs::File, str::FromStr};
+use async_minecraft_ping::{connect, StatusResponse, ServerDescription, ServerPlayers};
+use serde::{Deserialize};
+use uuid::{Uuid};
 
 #[tokio::main]
 async fn main() -> Result<()>{
@@ -7,17 +9,81 @@ async fn main() -> Result<()>{
     Ok(())
 }
 
-// let's presume that we already have json scan data from masscan
+// Let's presume that we already have json scan data from masscan
 
 async fn ingest_masscan_data() -> Result<Vec<MinecraftServer>> {
     let scan = File::open("scan.json")?;
     let scanned_servers: Vec<PingedServer> = serde_json::from_reader(scan)?;
-    println!("Scanned servers: {:?}", scanned_servers);
+    
+
 
     return Ok(Vec::new());
 }
 
-struct MinecraftServer;
+async fn ping_server(target: PingedServer) -> Result<MinecraftServer> {
+    if let Ok(connection) = connect(target.ip.clone()).await {// TODO: Tweak connection config
+        if let Ok(ping) = connection.status().await {
+            return Ok(MinecraftServer::from_status(ping.status, target.ip));
+        }
+    }
+    return Err(Error::new(ErrorKind::NotFound, format!("A server could not be reached")))
+}
+#[derive(Debug)]
+struct MinecraftServer {
+    ip: String,
+    version: String,
+    motd: String,
+    max_players: u32,
+    players: Vec<Player>,
+    /* 
+        Future ideas: 
+            - enforces-secure-profile
+            - Seperate version and server type
+    */
+
+}
+
+impl MinecraftServer {
+    fn from_status(status: StatusResponse, ip: String) -> MinecraftServer {
+        let mut description;
+        match status.description {
+            ServerDescription::Plain(text) => description = text,
+            ServerDescription::Object { text } => description = text,
+        }
+        return MinecraftServer { 
+            ip: ip,
+            version: status.version.name, 
+            motd: description, 
+            max_players: status.players.max, 
+            players: Player::from_server_players(status.players), 
+        }
+    }
+
+}
+
+#[derive(Debug)]
+struct Player {
+    username: String,
+    uuid: Uuid
+}
+
+impl Player {
+    fn from_server_players(server_players: ServerPlayers) -> Vec<Player> {
+        let mut players: Vec<Player> = Vec::new();
+        if let Some(server_player_vec) = server_players.sample {
+            for player in server_player_vec.iter() {
+                if let Ok(uuid) = Uuid::from_str(player.id.as_str()) {
+                    players.push(Player::new(player.name.clone(), uuid))
+                }
+            }
+        }
+        return players
+    }
+
+    fn new(username: String, uuid: Uuid) -> Player {
+        return Player { username: username, uuid: uuid }
+    }
+}
 
 #[derive(Debug, Deserialize)]
 struct PingedServer {
